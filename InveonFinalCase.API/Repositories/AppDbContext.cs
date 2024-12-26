ba524b0a923d7fb4bca20e1787ad25c1ab65fb1b
kemalege <cirpanliege@gmail.com>
2024-12-26 17:59:52 +0300

    Add custom AppUser with Identity integration, install dependencies

---
+++ InveonFinalCase.API/Repositories/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using InveonFinalCase.API.Domain.Entities;
+
+namespace InveonFinalCase.API.Repositories
+{
+    public class AppDbContext : IdentityDbContext<AppUser, IdentityRole, string>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+
+        {
+        }
+
+        public DbSet<UserRefreshToken> UserRefreshTokens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
